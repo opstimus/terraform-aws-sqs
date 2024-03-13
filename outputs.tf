653d18e131ff556084e24e2664ae7490cbfa6104
mfmsuhail <mfmsuhail@gmail.com>
2024-03-13 23:32:23 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -13,3 +13,29 @@ output "dlq_sqs_arn" {
 output "dlq_sqs_url" {
   value = var.enable_dlq == true ? aws_sqs_queue.dlq[0].url : null
 }
+
+resource "aws_ssm_parameter" "sqs_arn" {
+  name  = "/${var.project}/${var.environment}/central/sqs/sqs_arn"
+  type  = "String"
+  value = aws_sqs_queue.main.arn
+}
+
+resource "aws_ssm_parameter" "sqs_url" {
+  name  = "/${var.project}/${var.environment}/central/sqs/sqs_url"
+  type  = "String"
+  value = aws_sqs_queue.main.url
+}
+
+resource "aws_ssm_parameter" "dlq_sqs_arn" {
+  count = var.enable_dlq ? 1 : 0
+  name  = "/${var.project}/${var.environment}/central/sqs/dlq_sqs_arn"
+  type  = "String"
+  value = aws_sqs_queue.dlq[0].arn
+}
+
+resource "aws_ssm_parameter" "dlq_sqs_url" {
+  count = var.enable_dlq ? 1 : 0
+  name  = "/${var.project}/${var.environment}/central/sqs/dlq_sqs_url"
+  type  = "String"
+  value = aws_sqs_queue.dlq[0].url
+}
